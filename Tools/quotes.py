2d900900581ab6902178749431d0e71696b8ac9a
ShreyashKrishna <shreyash.krishna@ashoka.edu.in>
2017-10-27 18:39:40 +0530

    Updating revised version of quotes

--- Tools/quotes.py
+++ Tools/quotes.py
@@ -1,21 +1,17 @@
-filename = input("Enter the full path of the file to be used: ")  # enter the file name
-input1 = open(filename, 'r')
 import re
 
-str1 = input1.read()  # the entire file is stored in str1 variable
-# print(str1)
-quotes = re.findall(r'"[^"]*"', str1, re.U)  # adds all quotations with ''
-quotes1 = re.findall(r"'[^']*'", str1, re.U)  # adds all quotations with ""
-finallist = []
-for i in quotes:
-    i = i[:-1]
-    i = i[1:]
-    finallist.append(i)
-for j in quotes1:
-    j = j[:-1]
-    j = j[1:]
-    finallist.append(j)
-print(finallist)  # final list is entered.
+
+def quotes(str1):
+    finallist=[]
+    quotes1=re.findall(r'\"(.+?)\"',str1)#Regex Pattern match for all double quotes
+    #print(str1)
+    for i in quotes1:
+        i=i[:-1]#Remove the quotation marks from each string
+        i=i[1:]
+        finallist.append(i)
+    print(finallist)#output
+    print(len(finallist))
+    return str1;
 
 
 def remove_quotes(input_string):
