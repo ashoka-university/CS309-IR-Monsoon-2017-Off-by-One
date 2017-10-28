b5ce2fb1217e854054a8bcd40b408105dd9a0303
deva-ashoka <deva211196@gmail.com>
2017-10-28 08:37:12 +0530

    grammar check

---
+++ Tools/grammar.py
@@ -0,0 +1,20 @@
+import language_check
+import nltk
+
+
+def check_grammar_of_str(string):
+    tool = language_check.LanguageTool('en-US')
+    matches = tool.check(string)
+    score = 0
+    for match in matches:
+        if not match.ruleId == "MORFOLOGIK_RULE_EN_US":  # do not count any spell errors
+            score += 1
+    return score
+
+
+def check_grammar(string):
+    sentences = nltk.sent_tokenize(string)
+    score = 0
+    for sentence in sentences:
+        score += check_grammar_of_str(sentence)
+    return score
