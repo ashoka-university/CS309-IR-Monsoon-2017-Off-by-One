07804ffdd0ec8d1e1fb781aa7301df5a3e86399c
ayushree <31218565+ayushree@users.noreply.github.com>
2017-10-29 20:25:57 +0530

    adding long_sentences_average_length

--- Tools/words_per_sentence.py
+++ Tools/words_per_sentence.py
@@ -1,7 +1,22 @@
 from Tools import word_count
 from Tools import sentence_count
 
+
 def words_per_sentence(text):
     words = word_count.word_count(text)
     sentences = sentence_count.sentence_count(text)
-    return (words/sentences)
+    return (words / sentences)
+
+
+def long_sentences_average_length(sentences, average_sentence_length):
+    long_sentence_length_avg_value = 0
+    sentence_length = []
+    for sentence in sentences:
+        sentence_length.append(word_count.word_count(sentence))
+    long_sentences_count = 0
+    for length in sentence_length:
+        if length > average_sentence_length:
+            long_sentences_count += 1
+            long_sentence_length_avg_value += length
+    long_sentence_length_avg_value /= long_sentences_count
+    return long_sentence_length_avg_value
