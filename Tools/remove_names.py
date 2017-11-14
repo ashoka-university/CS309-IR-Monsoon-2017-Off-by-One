74a182896376ab069bd34be924c7dc72183a549d
ShreyashKrishna <shreyash.krishna@ashoka.edu.in>
2017-11-14 16:11:29 +0530

    Remove @

--- Tools/remove_names.py
+++ Tools/remove_names.py
@@ -1,11 +1,16 @@
-def remove_names(s):
-    a=s.split()
-    l=[]
+import re
+def remove_names(essay):
+    a=essay.split()
+    l=""
     for i in a:
         if(i[0]!='@'):
-            l.append(i)
-    print(l)
+            l=l+i+" "
+    return(l)
+    #words_noun=re.findall(r'@\w+\s+',essay_string,re.U)
+    #for element in essay_string:
+    #    essay_string.replace(element,'')
+    #print(essay_string)
 
-remove_names("Shreyash krishna @Eating House") 
-
+s=remove_names("@Redelmeier, Donald A., @and Robert J. Tibshirani. @Association @betweend Journal of @Medicine 336 (1997): 453-58.") 
+print(s)
         
